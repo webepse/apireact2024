1dfaba69b334c640ae6f11d9eb392a9fac9ae1de
berti <berti@CLASSES.LOCAL>
2024-01-16 09:26:44 +0100

    listing et routage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage = (props) => {
+    return ( 
+        <div className="jumbotron">
+            <h1 className="display-3">Hello World!</h1>
+            <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas temporibus numquam praesentium vel autem dolore magni at ipsa expedita possimus.</p>
+            <hr className="my-4" />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, possimus!</p>
+            <p className="lead">
+                <a href="#" className="btn btn-primary btn-lg">Learn More</a>
+            </p>
+        </div>
+     );
+}
+ 
+export default HomePage;
